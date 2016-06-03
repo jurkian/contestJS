59f1f8d43f7fe2b3880231eb538d0e5b3f62f121
Jakub Jurkian <jakub@jurkian.com>
2016-06-03 17:55:22 +0200

    Add jQuery, prepare routing and some views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,49 @@
+var $ = require('jquery'),
+	Page = require('page'),
+	View = require('./view.js');
+
 // Run the game if all assets are loaded
 var init = function() {
 
 	// Settings
-	var d = document,
-		settings = {
+	// ...
 
-		};
+	// Routing
+	View.init(document.querySelector('section.view'));
+	Page.base('/');
+
+	Page('/', function() {
+		View.load('views/intro.html');
+	});
+
+	Page('prizes', function() {
+		View.load('views/prizes.html');
+	});
+
+	Page('rules', function() {
+		View.load('views/rules.html');
+	});
+
+	Page('help', function() {
+		View.load('views/help.html');
+	});
+
+	Page('about', function() {
+		View.load('views/about.html');
+	});
+
+	Page('contact', function() {
+		View.load('views/contact.html');
+	});
+
+	// Questions
+	Page('question/:id', function(ctx, next) {
+		var id = ctx.params.id;
+	});
+
+	// Not found
+	Page('*', function() {});
+	Page();
 };
-window.addEventListener('load', init, false);
+
+$(window).on('load', init);
